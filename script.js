9497a357db5441d2fc3017285d380a21a212c122
Guest <guest-ctrmdr@h24497.sgb.skir.kr>
2021-03-20 23:01:02 +0900

    0320-2

--- script.js
+++ script.js
@@ -12,15 +12,13 @@ function initBoard() {
     board.fill(0);
     do {
         let ran = Math.floor(Math.random() * rows * cols); /*랜덤지뢰위치*/
-        //if (check(ran/rows,ran%rows)===0) {
             board[ran]=9;
             mineNum++;
-        //}
     } while (mineNum < mineCount);
 
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
-            if(board[i*rows+j]==9)
+            if(board[i*cols+j]==9)
                 continue;
             let minesAround = 0;
             minesAround += check(i - 1, j - 1);
@@ -31,7 +29,7 @@ function initBoard() {
             minesAround += check(i - 1, j + 1);
             minesAround += check(i, j + 1);
             minesAround += check(i + 1, j + 1);
-            board[i * rows + j] = minesAround;
+            board[i * cols + j] = minesAround;
         }
     }
 }
@@ -39,7 +37,7 @@ function initBoard() {
 function check(i,j){
     if(i<0 || i>=rows || j<0 || j>=cols)
         return 0;
-    else if(board[i*rows+j]==9)
+    else if(board[i*cols+j]==9)
         return 1; //숫자가 9면 지뢰. 지뢰면 개수 +1
     else return 0;
 
@@ -51,50 +49,38 @@ function initHTML() {
     for (let i = 0; i < rows; i++) {
         var newTr = document.createElement('tr');
         for (let j = 0; j < cols; j++) {
-            var newTd = document.createElement('td');
-            // if(board[i*rows+j]==9)
-            //     newTd.textContent="※";
-            // else if(board[i*rows+j]!=0)
-            //     newTd.textContent=board[i*rows+j];
-            
+            var newTd = document.createElement('td');       
             newTd.setAttribute("class","closed");
-            newTd.setAttribute("ind",i*rows+j);
+            newTd.setAttribute("ind",i*cols+j);
             newTd.addEventListener("click",clicked);
             newTr.appendChild(newTd);
         }
         tbody.appendChild(newTr);
     }
-
 }
-
-
-
-// function createButton(){
-//     var cells=document.querySelectorAll('td');
-//     cells.forEach((c)=>{
-//         var btn=document.createElement('div');
-//         btn.setAttribute("class","cellButton");
-//         btn.addEventListener("click",open);
-//         c.appendChild(btn);
-//     });
-// }
 
 function clicked(e){
     var target=e.target;
     var index=target.getAttribute("ind");
-    open(parseInt(index/rows), index%rows);
+    if(board[index]==9){
+        gameover(target);
+        return;
+    }
+
+    open(parseInt(index/cols), index%cols);
 }
 
 function open(row, col){
+
     if(row<0 || row>=rows || col<0 || col>=cols)
         return;
     var trList=document.getElementById("board_tbody").querySelectorAll('tr');
     var target=trList[row].querySelectorAll('td')[col];
-    if(target.getAttribute("class")=="open")
+    //console.log(target);
+    if(target.getAttribute("class")!="closed")
         return;
-    var num=board[row*rows+col];
+    var num=board[row*cols+col];
     if(num==9){
-        gameover();
     }
     else if(num>0 && num<9){
         target.setAttribute("class","open");
@@ -113,6 +99,27 @@ function open(row, col){
     }
 }
 
-function gameover(){
+function gameover(target){
     console.log("gameover");
+
+    target.setAttribute("class","mine");
+    target.textContent="※";
+
+    /*open all*/
+    var trList=document.getElementById("board_tbody").querySelectorAll('tr');
+    for(let i=0;i<board.length;i++){
+        var cell=trList[parseInt(i/cols)].querySelectorAll('td')[i%cols];
+        var num=board[cell.getAttribute("ind")];
+        if(num==9){
+            cell.setAttribute("class","mine");
+            cell.textContent="※";
+        }
+        else if(num>0 && num<9){
+            cell.setAttribute("class","open");
+            cell.textContent=num;
+        }
+        else if(num==0){
+            cell.setAttribute("class","open");
+        }
+    }
 }
